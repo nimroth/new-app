20f5c13b91115c0335b32df3a8c3c56a05fb8d94
Nimrothson J <nimrothson.johnson@ameexusa.com>
2019-06-27 20:39:33 +0530

    Added list and toolbar in the dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { MatSidenavModule, MatDividerModule, MatListModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [ DashboardComponent ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatListModule,
+    MatToolbarModule
   ]
 })
 export class DashboardModule { }

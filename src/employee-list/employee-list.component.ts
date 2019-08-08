e460dc79f154a78197f7037de1a193cb164bad22
Nimrothson J <nimrothson.johnson@ameexusa.com>
2019-08-08 14:38:10 +0530

    Added role column in employee list component

--- src/employee-list/employee-list.component.ts
+++ src/employee-list/employee-list.component.ts
@@ -3,7 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import * as Model from './model/employee-list.model';
-import * as RegisterModel from '../model/register.response';
+import * as RegisterModel from '../model/register';
 import { WebService } from '../services/web.service';
 import { RegisterService } from '../services/register.service';
 
@@ -14,9 +14,9 @@ import { RegisterService } from '../services/register.service';
 })
 export class EmployeeListComponent implements OnInit {
 
-  ELEMENT_DATA: RegisterModel.RegistrationDetails[] = [];
-  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'details'];
-  dataSource = new MatTableDataSource<RegisterModel.RegistrationDetails>(this.ELEMENT_DATA);
+  ELEMENT_DATA: RegisterModel.EmpListData[] = [];
+  displayedColumns: string[] = ['id', 'role', 'firstName', 'lastName', 'email', 'details'];
+  dataSource = new MatTableDataSource<RegisterModel.EmpListData>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
@@ -27,15 +27,13 @@ export class EmployeeListComponent implements OnInit {
               ) { }
 
   ngOnInit() {
-    // this.webService.getRequest('/employeeData', null).subscribe((data: RegisterModel.RegistrationDetails[]) => {
-    //   this.dataSource = new MatTableDataSource(data);
-    //   if (this.sort && this.paginator) {
-    //     this.dataSource.sort = this.sort;
-    //     this.dataSource.paginator = this.paginator;
-    //   }
-    //   console.log('list data = ', this.dataSource);
-    // });
-    this.registerService.getEmployee().subscribe((data: RegisterModel.RegistrationDetails[]) => {
+    this.registerService.getUser().subscribe((data: RegisterModel.EmpListData[]) => {
+      console.log('list data = ', data);
+      // if (data.role === 1) {
+      //   data.role = 'Admin';
+      // } else {
+      //   data.role = 'User';
+      // }
       this.dataSource = new MatTableDataSource(data);
       if (this.sort && this.paginator) {
         this.dataSource.sort = this.sort;
